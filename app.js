919d290530a790cc5bf66c2d064ff3e2c3f55667
KaifShaikh08~ <jeelanikaif250@gmail.com>
2023-07-22 03:50:55 -0700

    Setting Production Environment

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import taskRouter from "./Routes/task.js";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
 
 export const app = express();
 
@@ -14,6 +15,13 @@ config({
 //Using Middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tasks", taskRouter);
 
